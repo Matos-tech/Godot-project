6369551c01e48c6dd157cb8b80e5be920168cd75
@Igor-Y2 <Igory-byte@hotmail.com>
2025-12-01 10:56:20 -0300

    Paralax e movimentacao

--- novo-projeto-de-jogo-2D/novo-projeto-de-jogo/Scripts/Movimentacao.gd
+++ novo-projeto-de-jogo-2D/novo-projeto-de-jogo/Scripts/Movimentacao.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_FORCE= -400.0
+const SPEED = 150.0
+const JUMP_FORCE= -300.0
 
-@onready var animation:=$AnimatedSprite2D as AnimatedSprite2D
+@onready var animation:= $AnimatedSprite2D as AnimatedSprite2D
+var is_jumping := false
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -12,14 +13,24 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
-
+		is_jumping = true
+	elif  is_on_floor():
+		is_jumping=false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction !=0:
 		velocity.x = direction * SPEED
-		animation. play("idle")
+		animation.scale.x =-direction
+		if !is_jumping:
+			animation. play("run")
+			
+	elif is_jumping:
+		animation. play("jump")
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
+		
 
 	move_and_slide()
