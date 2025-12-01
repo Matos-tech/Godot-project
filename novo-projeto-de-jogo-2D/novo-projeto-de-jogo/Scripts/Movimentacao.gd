76a7e4b78b2cbb3ce181bd559055656653c4cdc6
@Igor-Y2 <Igory-byte@hotmail.com>
2025-12-01 00:49:13 -0300

    Movimentacao 2
    2

--- novo-projeto-de-jogo-2D/novo-projeto-de-jogo/Scripts/Movimentacao.gd
+++ novo-projeto-de-jogo-2D/novo-projeto-de-jogo/Scripts/Movimentacao.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_FORCE= -400.0
 
-
+@onready var animation:=$AnimatedSprite2D as AnimatedSprite2D
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -16,8 +16,9 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction !=0:
 		velocity.x = direction * SPEED
+		animation. play("idle")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
